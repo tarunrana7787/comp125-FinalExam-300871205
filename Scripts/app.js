2eb397114a6b0e8c44598066c1f6c366cab99c00
tarunrana7787 <tarunrana7787@gmail.com>
2016-08-17 14:00:00 -0400

    commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,6 +9,9 @@ var core;
     "use strict";
     var canvas;
     var stage;
+    var rollButton;
+    var blank;
+    var die1;
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
@@ -21,6 +24,10 @@ var core;
     function gameLoop() {
         stage.update();
     }
+      function rollButton_clicked() {
+        die1 = Math.floor((Math.random() * 6) + 1);
+        console.log(die1);
+
     function main() {
         //roll button
         rollButton = new createjs.Bitmap("Assets/images/rollButton.png");
@@ -28,6 +35,17 @@ var core;
         rollButton.y = 220;
         stage.addChild(rollButton);
     }
+    var Diceblank = new createjs.Bitmap("../Assets/images/blank.png");
+        var Dice = new createjs.Bitmap("../Assets/images/blank.png");
+        var Dice1 = new createjs.Bitmap("../Assets/images/1.png");
+        var Dice2 = new createjs.Bitmap("../Assets/images/2.png");
+        var Dice3 = new createjs.Bitmap("../Assets/images/3.png");
+        var Dice4 = new createjs.Bitmap("../Assets/images/4.png");
+        var Dice5 = new createjs.Bitmap("../Assets/images/5.png");
+        var Dice6 = new createjs.Bitmap("../Assets/images/6.png");
+        var rollButton = new createjs.Bitmap("../Assets/images/rollButton.png");
+        var Buttonhelper = new createjs.ButtonHelper(rollButton,"normal","hover","clicked");
+        rollButton.addEventListener('clicked', roll);
     window.addEventListener("load", init);
 })(core || (core = {}));
 //# sourceMappingURL=app.js.map
